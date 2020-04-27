951f89b6abccce98ccebcf17c7c68345fdd184f0
pavan-elisetty <57026704+pavan-elisetty@users.noreply.github.com>
2020-04-27 11:18:47 +0530

    Update bot.py
    in updated version of sklearn it is replaced by model_selection

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.tree import DecisionTreeClassifier,_tree
 import numpy as np
-from sklearn.cross_validation import train_test_split
-from sklearn import cross_validation
+from sklearn.model_selection import train_test_split
+from sklearn import model_selection
 from sklearn.tree import export_graphviz
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
